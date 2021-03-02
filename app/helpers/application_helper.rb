ce15dc10a1f42c50ecc86f107bf0acc3bd95b80c
kizumo25 <kizumo25@gmail.com>
2021-03-02 16:24:45 +0900

    Add helper 'full_title'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = "My beans"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,29 +3,30 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 	def setup
-		@base_title = " My beans"
+		@base_title = "My beans"
 	end
 
 	test "should get root" do
 		get static_pages_home_url
 		assert_response :success
+		assert_select "title", "#{@base_title}"
 	end
 
   test "should get home" do
     get static_pages_home_url
 		assert_response :success
-		assert_select "title", "Home |#{@base_title}"
+		assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
 		assert_response :success
-		assert_select "title", "About |#{@base_title}"
+		assert_select "title", "About | #{@base_title}"
   end
 
 	test "should get contact" do
 		get static_pages_contact_url
 		assert_response :success
-		assert_select "title", "Contact |#{@base_title}"
+		assert_select "title", "Contact | #{@base_title}"
 	end
 end
