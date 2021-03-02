90b5d4962352d60e4171693a412ada9abda24565
kizumo25 <kizumo25@gmail.com>
2021-03-02 14:42:10 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title = " My beans"
 	end
 
+	test "should get root" do
+		get static_pages_home_url
+		assert_response :success
+	end
+
   test "should get home" do
     get static_pages_home_url
 		assert_response :success
