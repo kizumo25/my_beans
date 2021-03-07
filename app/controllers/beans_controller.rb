c5df633c48b2db83b2b5f5b194e79d806121a8bb
kizumo25 <kizumo25@gmail.com>
2021-03-07 20:34:50 +0900

    Imrove view

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -3,12 +3,22 @@ class BeansController < ApplicationController
 		@beans = Bean.all
 	end
 
+	def show
+	end
+
 	def new
 		@bean = Bean.new
 	end
 
+	def edit
+	end
+
 	def create
-		@bean = Bean.create
+		@bean = Bean.new
+		if @bean.save
+			flash[:success] = "あなたのコーヒー豆が登録されました！"
+			redirect_to '/beans'
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'beans/index'
-
 	root 'static_pages#home'
 
 	get 'static_pages/home'
 	get 'static_pages/about'
 	get 'static_pages/contact'
+
+	resources :beans
 end
