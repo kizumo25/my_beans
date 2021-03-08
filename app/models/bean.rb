8ee831e06d7f0594abd69397665addbdc30bd701
kizumo25 <kizumo25@gmail.com>
2021-03-08 13:15:51 +0900

    Add picture uploader

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,4 +1,13 @@
 class Bean < ApplicationRecord
 	validates :name, presence: true, length: {maximum: 20}
 	mount_uploader :picture, PictureUploader
+	validate :picture_size
+
+	private
+
+		def picture_size
+			if picture.size > 5.megabytes
+				errors.add(:picture, "should be less than 5MB")
+			end
+		end
 end
